2e23b4bd804d83fa210312dbde54035d2ca01feb
如风过境 <lpyr@outlook.com>
2026-06-07 14:22:56 +0800

    docs(english): add daily lesson 2026-06-07

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -58,7 +58,7 @@ export default defineConfig({
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
-      { text: "English Daily", link: "/english/2026-06-06" }
+      { text: "English Daily", link: "/english/2026-06-07" }
     ],
     sidebar: [
       {
